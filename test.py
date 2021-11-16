aef1e8da99e4912bb1cbcb43e611830909cd4021
groiq <35578027+groiq@users.noreply.github.com>
2021-11-16 18:55:03 +0100

    qr code can be read from external file; supports multiple codes

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from base45 import b45decode, b45encode
+from base45 import b45decode
 from cose.messages import CoseMessage
 from pprint import pprint
 
@@ -7,13 +7,23 @@ import cbor2
 import json
 
 def main():
-    input = b'HC1:6BFNX1:HM*I0PS3TLU.NGMU5AG8JKM:SF9VN1RFBIKJ:3AXL1RR+ 8::N$OAG+RC4NKT1:P4.33GH40HD*98UIHJIDB 4N*2R7C*MCV+1AY3:YP*YVNUHC.G-NFPIR6UBRRQL9K5%L4.Q*4986NBHP95R*QFLNUDTQH-GYRN2FMGO73ZG6ZTJZC:$0$MTZUF2A81R9NEBTU2Y437XCI9DU 4S3N%JRP:HPE3$ 435QJ+UJVGYLJIMPI%2+YSUXHB42VE5M44%IJLX0SYI7BU+EGCSHG:AQ+58CEN RAXI:D53H8EA0+WAI9M8JC0D0S%8PO00DJAPE3 GZZB:X85Y8345MOLUZ3+HT0TRS76MW2O.0CGL EQ5AI.XM5 01LCWBA.RE.-SUYH+S7SBE0%B-KT+YSMFCLTQQQ6LEHG.P46UNL6DA2C$AF-SQ00A58HYO5:M8 7S$ULGC-IP49MZCSU8ST3HDRJNPV3UJADJ9BVV:7K13B4WQ+DCTEG4V8OT09797FZMQ3/A7DU0.3D148IDZ%UDR9CYF'
+    with open('./vaccinationCodes.json', 'r', encoding='UTF-8') as infile:
+        vaccinationCodes = json.load(infile)
+    # pprint(vaccinationCodes)
 
-    rawData = decodeQR(input)
-    # pprint(rawData)
+    for code in readCodesFromFile('./samples/sampleQRCodes.txt'):
+        rawData = decodeQR(code)
+        # pprint(rawData)
 
-    extractedData = extractRelevantJsonFields(rawData)
-    pprint(extractedData)
+        extractedData = extractRelevantJsonFields(rawData, vaccinationCodes)
+        pprint(extractedData)
+
+def readCodesFromFile(path):
+    with open(path, 'r', encoding='UTF-8') as infile:
+        for line in infile:
+            if not line:
+                continue
+            yield line.rstrip().encode()
 
 def decodeQR(input):
     left, right = input[:4], input[4:]
@@ -25,11 +35,7 @@ def decodeQR(input):
     processed = CoseMessage.decode(processed)
     return cbor2.loads(processed.payload)
 
-def extractRelevantJsonFields(rawData):
-    with open('./vaccinationCodes.json', 'r', encoding='UTF-8') as infile:
-        vaccinationCodes = json.load(infile)
-    # pprint(vaccinationCodes)
-
+def extractRelevantJsonFields(rawData, vaccinationCodes):
     extractedData = {}
 
     vaccine = rawData[-260][1]['v'][0]
